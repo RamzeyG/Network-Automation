f169c34369359daa2ed9e944bd824587f271431d
Ramzey Ghanaim <rghanaim@intervision.com>
2018-09-26 16:36:26 -0700

    added a device type file, and simplified some code

---
+++ devicetype.py
@@ -0,0 +1,22 @@
+#     device_type.py
+#
+# This file contains the list of supported devices,
+# and commands used by each device
+
+
+# Commands to get into config mode
+config_mode = {
+		'cisco': 'en\nconf t\nterm len 0',
+		'arista': 'en\nconf t\nterm len 0',
+		'juniper': 'cli\nconfigure\nset cli screen-length 0',
+		'pan': 'configure\nset cli pager off'
+}
+
+# keys used to look for "wrong command"
+invalid_cmd_key = {
+	'cisco': 'Invalid',
+	'arista': 'Invalid',
+	'juniper': '',
+	'pan': 'Unknown command:',
+	'ubuntu': 'not found'
+}
