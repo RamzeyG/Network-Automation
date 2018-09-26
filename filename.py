44663ac26d3b9c0dc174a98cf7083b6368aa7547
Ramzey Ghanaim <rghanaim@intervision.com>
2018-09-26 15:38:35 -0700

    Seperated code that grabs the hostname ino its own file

---
+++ filename.py
@@ -0,0 +1,40 @@
+
+def get_final_hostname(hostname, hostname_found, new_file):
+
+    if '# ' in hostname:
+        rm_prompt = hostname.split('#')
+        hostname = rm_prompt[0]
+        hostname_found = True
+    elif '> ' in hostname:
+        rm_prompt = hostname.split('>')
+        hostname = rm_prompt[0]
+        hostname_found = True
+    elif '$ ' in hostname:
+        rm_prompt = hostname.split('$')
+        hostname = rm_prompt[0]
+        hostname_found = True
+        print 'here!'
+
+    # Look for" hostname(currentmode)"
+    if '(' in hostname:
+        rm_parenthesis = hostname.split('(')
+        hostname = rm_parenthesis[0]
+
+    # Look for username@hostname
+    if '@' in hostname:
+        rm_user = hostname.split('@')
+        hostname = rm_user[1]
+        hostname = hostname.strip('~').strip(':').strip()
+
+    # on linux machines: for "hostname:directory"
+    elif ':' in hostname:
+        rm_user = hostname.split(':')
+        hostname = rm_user[0]
+    if hostname_found:
+        new_file += '-' + hostname + '.txt'
+        result = open(new_file, 'w')
+    if not hostname_found:
+        print 'DID NOT FIND HOSTNAME, exiting program'
+        exit()
+
+    return new_file, result
